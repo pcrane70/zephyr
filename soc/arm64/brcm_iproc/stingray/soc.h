8f958a71133f48869aba4374c8cca4f909237e0f
Abhishek Shah <abhishek.shah@broadcom.com>
2019-10-25 15:08:03 +0530

    boards: Add support for stingray SoC and bcm958742k board
    Add support for stingray SoC and combo svk (bcm958742k) board.
    This is made available for early development, and is expected
    to be removed later.
    
    Signed-off-by: Abhishek Shah <abhishek.shah@broadcom.com>

---
+++ soc/arm64/brcm_iproc/stingray/soc.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2019 Broadcom.
+ * The term “Broadcom” refers to Broadcom Inc. and/or its subsidiaries
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC_SR_H_
+#define _SOC_SR_H_
+
+#ifndef _ASMLANGUAGE
+#include <device.h>
+#endif
+
+/* Add SOC registers here as required */
+
+
+#endif /* _SOC_SR_H_ */
